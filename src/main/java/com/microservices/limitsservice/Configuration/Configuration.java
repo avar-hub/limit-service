0d0b893f9b05ae712f967d7b392b7f0295aa9bdb
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 <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>
2024-03-01 18:29:42 +0530

    fetching properties from central config server

--- src/main/java/com/microservices/limitsservice/Configuration/Configuration.java
+++ src/main/java/com/microservices/limitsservice/Configuration/Configuration.java
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties("limits-service")
+@ConfigurationProperties("limits-service") // -> fetching data from properties
 @Getter
 @Setter
 public class Configuration {
